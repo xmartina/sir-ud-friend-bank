9ad6e65ee429debb00241094d9c9626f0ad2ea5b
unknown <martinsprince126@gmail.com>
2023-06-20 10:46:17 +0100

    Modified: SMTP -> user matagram SMTP Details Instead

--- include/smtp.php
+++ include/smtp.php
@@ -10,18 +10,18 @@ class message{
         $mail = new PHPMailer();
         //SMTP Settings
         $mail->isSMTP();
-        $mail->Host = "mail.matagram.com"; // Change Email Host
+        $mail->Host = "mail.cryptogiant.biz"; // Change Email Host
         $mail->SMTPAuth = true;
-        $mail->Username = "support@matagram.com"; // Change Email Address
+        $mail->Username = "test@cryptogiant.biz"; // Change Email Address
         $mail->Password = '+C@ppy126'; // Change Email Password
         $mail->Port = 587; //465; //587
         $mail->SMTPSecure = "ssl"; //tls
 
         //Email Settings
         $mail->isHTML(true);
-        $mail->setFrom('info@fortageunion.com','Support Fortage Union'); // Change
+        $mail->setFrom('test@cryptogiant.biz','Support Fortage Union'); // Change
         $mail->addAddress($email);
-        $mail->AddReplyTo("info@fortageunion.com", "Support Fortage Union"); // Change
+        $mail->AddReplyTo("test@cryptogiant.biz", "Support Fortage Union"); // Change
         $mail->Subject = $subject;
         $mail->MsgHTML($message);
         $mail->Send();
